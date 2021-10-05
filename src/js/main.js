d05c96554611e22171dea74d682845d5f179e89f
milan.zivanov <milan.zivanov@gmail.com>
2021-10-05 08:45:08 +0200

    slider dots fixed for the desktop and up view

--- src/js/main.js
+++ src/js/main.js
@@ -54,6 +54,16 @@ jQuery(function() {
               dots: true,
               arrows: false
             }
+          },
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: false,
+              arrows: false
+            }
           }
         ]
     });
