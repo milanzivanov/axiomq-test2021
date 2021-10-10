bd142625891179483e37ab0668a6eee61acae357
milan.zivanov <milan.zivanov@gmail.com>
2021-10-10 12:28:43 +0200

    reorganization container become a landing-conatainer and some parts are now outside of the landing-container

--- src/js/main.js
+++ src/js/main.js
@@ -39,31 +39,34 @@ jQuery(function() {
           {
             breakpoint: 360,
             settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              infinite: true,
-              dots: true,
-              arrows: false
+                centerMode: false,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true,
+                arrows: false
             }
           },
           {
             breakpoint: 768,
             settings: {
-              slidesToShow: 3,
-              slidesToScroll: 1,
-              infinite: true,
-              dots: true,
-              arrows: false
+                centerMode: true,
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true,
+                arrows: false
             }
           },
           {
             breakpoint: 1024,
             settings: {
-              slidesToShow: 3,
-              slidesToScroll: 1,
-              infinite: true,
-              dots: false,
-              arrows: false
+                centerMode: true,
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false,
+                arrows: false
             }
           }
         ]
